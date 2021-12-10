670b7c8e60c782959114f9c83010d1cf39bcbe94
Matt Rantz <mjr445@drexel.edu>
2021-12-10 16:01:39 -0500

    Removed duplicates

--- day1/day1-1.py
+++
@@ -1,9 +0,0 @@
-with open("day1.txt", "r") as f:
-    numIncreases = 0
-    prevNum = float('inf')
-    for line in f:
-        curNum = int(line)
-        if curNum > prevNum:
-            numIncreases = numIncreases + 1
-        prevNum = curNum
-    print(numIncreases)
