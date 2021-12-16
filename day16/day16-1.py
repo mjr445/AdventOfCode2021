b210ae6ec9e9a3c648b4e97684d08c2392bd087a
Matt Rantz <43094069+mjr445@users.noreply.github.com>
2021-12-16 16:26:50 -0500

    Begin Day 16

---
+++ day16/day16-1.py
@@ -0,0 +1,90 @@
+def read_sub_packet(packet, starting_index, max_packet_length=None):
+
+    if max_packet_length:
+        max_index = max_packet_length + starting_index
+    
+    packet_version_index_end, packet_type_id_index_end = starting_index+3, starting_index+6
+    packet_version = int(packet[starting_index:packet_version_index_end], 2)
+    print(f"Packet Version: {packet_version}")
+    packet_type_id = int(packet[packet_version_index_end:packet_type_id_index_end], 2)
+
+    is_literal_value = packet_type_id == 4  # If not, it's an operator
+
+    if is_literal_value:
+
+        not_encountered_final_group = True
+
+        starting_index = packet_type_id_index_end
+        final_value = ""
+        final_sub_packet_length = 0
+
+        while not_encountered_final_group:
+            not_encountered_final_group = packet[starting_index] == "1"
+
+            starting_index = starting_index + 1
+            ending_index = starting_index + 4
+
+            if max_packet_length and ending_index > max_index:
+                ending_index = max_index
+                not_encountered_final_group = False
+            
+            final_value = final_value + packet[starting_index:ending_index]
+            final_sub_packet_length = final_sub_packet_length + 1 + ending_index - starting_index
+
+            starting_index = ending_index
+        
+        final_value = packet_version
+    
+    else:  # It's an operator
+        final_value = current_length_of_sub_packets = 0
+
+        length_type_id = packet[packet_type_id_index_end]
+        if length_type_id == "0":
+            length_of_sub_packets_index_start = packet_type_id_index_end + 1 
+            new_sub_packet_index_start = length_of_sub_packets_index_start + 15
+
+            max_length_of_sub_packets = int(packet[length_of_sub_packets_index_start:new_sub_packet_index_start], 2)  # Next 15 bits
+            while current_length_of_sub_packets != max_length_of_sub_packets:
+                sub_packet_value, sub_packet_length = read_sub_packet(packet, new_sub_packet_index_start, max_length_of_sub_packets - current_length_of_sub_packets)
+
+                final_value = final_value + sub_packet_value
+                current_length_of_sub_packets = current_length_of_sub_packets + sub_packet_length
+                new_sub_packet_index_start = new_sub_packet_index_start + sub_packet_length
+            
+            final_sub_packet_length = current_length_of_sub_packets + 15
+            
+        else:  # TODO: Confirm working
+            number_of_sub_packets_index_start = packet_type_id_index_end + 1 
+            new_sub_packet_index_start = number_of_sub_packets_index_start + 11
+            
+            number_of_sub_packets = int(packet[number_of_sub_packets_index_start:new_sub_packet_index_start], 2)  # Next 11 bits
+
+            print(f"Number of Sub Packets: {number_of_sub_packets}")
+            for i in range(number_of_sub_packets):
+                sub_packet_value, sub_packet_length = read_sub_packet(packet, new_sub_packet_index_start, max_packet_length)
+
+                final_value = final_value + sub_packet_value
+                current_length_of_sub_packets = current_length_of_sub_packets + sub_packet_length
+                new_sub_packet_index_start = new_sub_packet_index_start + sub_packet_length
+        
+            final_sub_packet_length = current_length_of_sub_packets + 11
+        
+        final_sub_packet_length = final_sub_packet_length + 1
+
+        final_value = final_value + packet_version
+    
+    final_sub_packet_length = final_sub_packet_length + 6
+    return final_value, final_sub_packet_length
+
+
+with open("day16.txt", "r") as file:
+    final_sum = 0
+    for packet in file:
+        packet = packet.strip()
+        packet = bin(int(packet, 16))[2:]  # Hex to Binary
+        print(f"Packet Rep: {packet}")
+
+        intermediate_sum, _ = read_sub_packet(packet, 0)
+        print(f"Intermediate Sum: {intermediate_sum}")
+        final_sum = final_sum + intermediate_sum
+    # print(final_sum)  TODO: Uncomment
